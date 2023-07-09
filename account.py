54aae8429a93541ee675e14783b527a77955f09a
ashwabu <abdulazizredi90@gmail.com>
2023-07-09 22:31:24 +0300

    writing in to balance

--- account.py
+++ account.py
@@ -1,5 +1,6 @@
 class Account:
     def __init__(self,fpath):
+        self.fpath = fpath
         with open(fpath,'r') as file:
             self.balance = int(file.read())
     
@@ -10,10 +11,13 @@ class Account:
     def deposit(self,amount):
         self.balance += amount
 
+    def commit(self):
+        with open(self.fpath, 'w') as file:
+            file.write(str(self.balance)) 
+
 
 account = Account('balance.txt')
 print(account.balance)
 account.withdraw(200)
 print(account.balance)
-account.deposit(200)
-print(account.balance)
+account.commit()
